041dadc1c6034635e1063a6c4abd93e8dfa8f237
Alghif04 <ibadjuga@gmail.com>
2024-05-28 19:23:31 +0700

    data dinamis diambil dari database

--- header.php
+++ header.php
@@ -22,32 +22,10 @@
             margin: 0;
             font-size: 24px;
         }
-        .search-bar {
-            display: flex;
-        }
-        .search-bar input[type="text"] {
-            padding: 6px;
-            border: 1px solid #5D639D;
-            border-radius: 4px;
-            background-color: #5D639D;
-        }
-        .search-bar button {
-            background-color: #fff;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            padding: 6px 10px;
-            margin-left: 10px;
-            cursor: pointer;
-        }
     </style>
 </head>
 <body>
     <div class="header">
-        <h1></h1>
-        <div class="search-bar">
-            <input type="text">
-            <button>Search</button>
-        </div>
     </div>
 </body>
 </html>
